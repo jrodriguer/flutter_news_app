72f62bd540d6d23dbbb0a10093f6bbdde7ef0a2e
Julio <46267103+jrodriguer@users.noreply.github.com>
2023-08-03 15:02:57 +0200

    setting up TabBar

--- lib/main.dart
+++ lib/main.dart
@@ -30,39 +30,26 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          bottom: const TabBar(tabs: [
+            Tab(icon: Icon(Icons.person_pin_circle_outlined)),
+            Tab(icon: Icon(Icons.circle)),
+            Tab(icon: Icon(Icons.heart_broken_outlined))
+          ]),
+          title: const Text('Tabs Demo'),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        // body: const TabBarView(
+        //   children: [
+        //     Text('Tabs Demo 1'),
+        //     Text('Tabs Demo 2'),
+        //     Text('Tabs Demo 3'),
+        //   ],
+        // ),
       ),
     );
   }
