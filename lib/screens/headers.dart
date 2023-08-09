59de930ed5fb3d4f7fa933e75069a41c3421eda7
Julio <46267103+jrodriguer@users.noreply.github.com>
2023-08-09 11:56:18 +0200

    add tabs for headers screen

--- lib/screens/headers.dart
+++ lib/screens/headers.dart
@@ -8,10 +8,42 @@ class HeadersScreeen extends StatefulWidget {
 }
 
 class _HeadersScreeenState extends State<HeadersScreeen> {
+  List<String> tabsText = <String>[
+    'business',
+    'entertainment',
+    'general',
+    'health',
+    'science',
+    'sports',
+    'technology'
+  ];
+  tabMaker() {
+    List<Tab> tabs = [];
+    for (var i = 0; i < tabsText.length; i++) {
+      tabs.add(Tab(
+        text: tabsText[i],
+      ));
+    }
+    return tabs;
+  }
+
+  String selectedCategory = '';
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Headers'),
+    return DefaultTabController(
+      initialIndex: 1,
+      length: tabsText.length,
+      child: Scaffold(
+        appBar: AppBar(
+          flexibleSpace: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              TabBar(tabs: tabMaker(), isScrollable: true),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
