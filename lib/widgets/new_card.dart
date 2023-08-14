89d72eba4d50e56e66fc77ceb6bff4f6b65cf938
jrodriguer <juliorodriguezramirez@gmail.com>
2023-08-14 18:21:26 +0200

    remove unnecessary files and refactor the import

--- lib/widgets/new_card.dart
+++ lib/widgets/new_card.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_news_app/constants/size_constants.dart';
-import 'package:flutter_news_app/constants/ui_constants.dart';
 import 'package:flutter_news_app/models/article.dart';
 
 class NewCard extends StatelessWidget {
@@ -17,19 +15,18 @@ class NewCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      elevation: Sizes.dimen_4,
+      elevation: 4.0,
       shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.all(Radius.circular(Sizes.dimen_10))),
-      margin: const EdgeInsets.fromLTRB(
-          Sizes.dimen_16, 0, Sizes.dimen_16, Sizes.dimen_16),
+          borderRadius: BorderRadius.all(Radius.circular(10.0))),
+      margin: const EdgeInsets.fromLTRB(16.0, 0, 16.0, 16.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
           ClipRRect(
               borderRadius: const BorderRadius.only(
-                  topLeft: Radius.circular(Sizes.dimen_10),
-                  topRight: Radius.circular(Sizes.dimen_10)),
+                  topLeft: Radius.circular(10.0),
+                  topRight: Radius.circular(10.0)),
               child: Image.network(
                 article.urlToImage,
                 height: 200,
@@ -41,7 +38,7 @@ class NewCard extends StatelessWidget {
                   return Card(
                     elevation: 0,
                     shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(Sizes.dimen_10)),
+                        borderRadius: BorderRadius.circular(10.0)),
                     child: const SizedBox(
                       height: 200,
                       width: double.infinity,
@@ -50,25 +47,24 @@ class NewCard extends StatelessWidget {
                   );
                 },
               )),
-          vertical15,
+          const SizedBox(height: 15.0),
           Padding(
-            padding: const EdgeInsets.all(Sizes.dimen_6),
+            padding: const EdgeInsets.all(6.0),
             child: Text(
               article.title,
               maxLines: 2,
               style: const TextStyle(
                   color: Colors.black87,
-                  fontSize: Sizes.dimen_20,
+                  fontSize: 20.0,
                   fontWeight: FontWeight.w500),
             ),
           ),
           Padding(
-            padding: const EdgeInsets.all(Sizes.dimen_6),
+            padding: const EdgeInsets.all(6.0),
             child: Text(
               article.description,
               maxLines: 2,
-              style: const TextStyle(
-                  color: Colors.black54, fontSize: Sizes.dimen_14),
+              style: const TextStyle(color: Colors.black54, fontSize: 14.0),
             ),
           )
         ],
