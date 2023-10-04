e97f737705821f711e64f052b3f807230494eea0
jrodriguer <juliorodriguezramirez@gmail.com>
2023-10-04 14:11:40 +0200

    add widget for show firebase messages and token into cards

--- lib/views/personal_screen.dart
+++ lib/views/personal_screen.dart
@@ -1,17 +1,41 @@
 import 'package:flutter/material.dart';
 
+import 'package:flutter_news_app/permissions.dart';
+import 'package:flutter_news_app/token_monitor.dart';
+import 'package:flutter_news_app/widgets/message_list.dart';
+import 'package:flutter_news_app/widgets/meta_card.dart';
+
 class PersonalScreen extends StatefulWidget {
-  const PersonalScreen({Key? key}) : super(key: key);
+  const PersonalScreen({super.key});
 
   @override
   State<PersonalScreen> createState() => _PersonalScreenState();
 }
 
 class _PersonalScreenState extends State<PersonalScreen> {
+  String? _token;
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Personal', style: TextStyle(fontSize: 16)),
+    return SingleChildScrollView(
+      child: Column(
+        children: <Widget>[
+          const MetaCard('Permissions', Permissions()),
+          MetaCard(
+            'FCM Token',
+            TokenMonitor((token) {
+              _token = token;
+              return token == null
+                  ? const CircularProgressIndicator()
+                  : SelectableText(
+                token,
+                style: const TextStyle(fontSize: 12),
+              );
+            }),
+          ),
+          const MetaCard('Message Stream', MessageList(),)
+        ],
+      )
     );
   }
 }
---
+++ lib/widgets/meta_card.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class MetaCard extends StatelessWidget {
+  final String _title;
+  final Widget _children;
+
+  const MetaCard(this._title, this._children, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      margin: const EdgeInsets.only(left: 8, right: 8, top: 8),
+      child: Card(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            children: <Widget>[
+              Container(
+                margin: const EdgeInsets.only(bottom: 16),
+                child: Text(_title, style: const TextStyle(fontSize: 16),),
+              ),
+              _children,
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
