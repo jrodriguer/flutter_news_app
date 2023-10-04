5090929af00752fd3b4263cb33c26162e217fe3d
jrodriguer <juliorodriguezramirez@gmail.com>
2023-10-04 18:07:49 +0200

    add switch for popup menu

--- lib/views/personal_screen.dart
+++ lib/views/personal_screen.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 import 'package:flutter_news_app/permissions.dart';
@@ -15,36 +17,106 @@ class PersonalScreen extends StatefulWidget {
 class _PersonalScreenState extends State<PersonalScreen> {
   String? _token;
 
+  Future<void> onActionSelected(String value) async {
+    switch (value) {
+      case 'subscribe':
+        {
+          print(
+              'FlutterFire Messaging Example: Subscribing to topic "fcm_test".');
+          await FirebaseMessaging.instance.subscribeToTopic('all_users');
+          print(
+              'FlutterFire Messaging Example: Subscribing to topic "all_users" successful.');
+        }
+        break;
+      case 'unsubscribe':
+        {
+          print(
+              'FlutterFire Messaging Example: Unsubscribing from topic "fcm_test".');
+          await FirebaseMessaging.instance.unsubscribeFromTopic('all_users');
+          print(
+              'FlutterFire Messaging Example: Unsubscribing from topic "all_users" successful.');
+        }
+        break;
+      case 'get_apns_token':
+        {
+          if (defaultTargetPlatform == TargetPlatform.iOS ||
+              defaultTargetPlatform == TargetPlatform.macOS) {
+            print('FlutterFire Messaging Example: Getting APNs token...');
+            String? token = await FirebaseMessaging.instance.getAPNSToken();
+            print('FlutterFire Messaging Example: Got APNs token: $token');
+          } else {
+            print(
+              'FlutterFire Messaging Example: Getting an APNs token is only supported on iOS and macOS platforms.',
+            );
+          }
+        }
+        break;
+      default:
+        break;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          'Personal',
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+        ),
+        actions: <Widget>[
+          PopupMenuButton(
+            // onSelected: onActionSelected,
+            itemBuilder: (BuildContext context) {
+              return [
+                const PopupMenuItem(
+                  value: 'subscribe',
+                  child: Text('Subscribe to topic'),
+                ),
+                const PopupMenuItem(
+                  value: 'unsubscribe',
+                  child: Text('Unsubscribe to topic'),
+                ),
+                const PopupMenuItem(
+                  value: 'get_apns_token',
+                  child: Text('Get APNs token (Apple only)'),
+                ),
+              ];
+            },
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
         child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              children: <Widget>[
-                const MetaCard(
-                  'Permissions',
-                  Permissions(),
-                ),
-                MetaCard(
-                  'FCM Token',
-                  TokenMonitor((token) {
-                    _token = token;
-                    return token == null
-                        ? const CircularProgressIndicator()
-                        : SelectableText(
-                            token,
-                            style: const TextStyle(
-                              fontSize: 14,
-                            ),
-                          );
-                  }),
-                ),
-                const MetaCard(
-                  'Message Stream',
-                  MessageList(),
-                )
-              ],
-            )));
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            children: <Widget>[
+              const MetaCard(
+                'Permissions',
+                Permissions(),
+              ),
+              MetaCard(
+                'FCM Token',
+                TokenMonitor((token) {
+                  _token = token;
+                  return token == null
+                      ? const CircularProgressIndicator()
+                      : SelectableText(
+                          token,
+                          style: const TextStyle(
+                            fontSize: 14,
+                          ),
+                        );
+                }),
+              ),
+              const MetaCard(
+                'Message Stream',
+                MessageList(),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
