97ecff4fc5fffd638ac148bf7b1b8d0117fa8274
jrodriguer <juliorodriguezramirez@gmail.com>
2023-10-11 19:15:04 +0200

    reformat dart code and use sizer package for media queries

--- lib/widgets/news_grid.dart
+++ lib/widgets/news_grid.dart
@@ -1,5 +1,6 @@
 import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 import 'package:flutter_news_app/models/article.dart';
 import 'package:flutter_news_app/services/data_local.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
@@ -35,7 +36,7 @@ class _NewsGridState extends State<NewsGrid> {
 
     return Container(
       padding: const EdgeInsets.symmetric(vertical: 30),
-      height: MediaQuery.of(context).size.height / 2,
+      height: 50.h,
       color: Colors.white,
       child: Column(
         children: [
@@ -52,10 +53,9 @@ class _NewsGridState extends State<NewsGrid> {
               ),
             ),
           ),
-          const SizedBox(height: 17),
           const Padding(
             padding: EdgeInsets.symmetric(
-              vertical: 7,
+              vertical: 30,
               horizontal: 15,
             ),
             child: Divider(
@@ -84,27 +84,29 @@ class _NewsGridState extends State<NewsGrid> {
                       title: Text('Copy Link'),
                     )),
                 GestureDetector(
-                    onTap: () async {
-                      if (widget.onFavorites) {
-                        // if (isFavorite) {
-                        //   await dataLocal.deleteNew(article);
-                        // } else {
-                        //   await dataLocal.saveNew(article);
-                        // }
-                        // ScaffoldMessenger.of(context).showSnackBar(
-                        //   SnackBar(
-                        //     content: Text(isFavorite ? 'Delete favorite' : 'Add to favorites'),
-                        //   ),
-                        // );
-                        setState(() {
-                          // favoriteStates[article.id] = !isFavorite;
-                        });
-                      }
-                      Navigator.of(context).pop();
-                    },
-                    child: const ListTile(
-                        leading: Icon(Icons.favorite_border_outlined),
-                        title: Text('Favorite')))
+                  onTap: () async {
+                    if (widget.onFavorites) {
+                      // if (isFavorite) {
+                      //   await dataLocal.deleteNew(article);
+                      // } else {
+                      //   await dataLocal.saveNew(article);
+                      // }
+                      // ScaffoldMessenger.of(context).showSnackBar(
+                      //   SnackBar(
+                      //     content: Text(isFavorite ? 'Delete favorite' : 'Add to favorites'),
+                      //   ),
+                      // );
+                      setState(() {
+                        // favoriteStates[article.id] = !isFavorite;
+                      });
+                    }
+                    Navigator.of(context).pop();
+                  },
+                  child: const ListTile(
+                    leading: Icon(Icons.favorite_border_outlined),
+                    title: Text('Favorite'),
+                  ),
+                )
               ],
             ),
           ),
@@ -132,13 +134,14 @@ class _NewsGridState extends State<NewsGrid> {
           return GestureDetector(
             onDoubleTap: () => _showNewsDetails(index),
             child: Container(
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.black,
-                  ),
+              padding: const EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black,
                 ),
-                child: Column(children: [
+              ),
+              child: Column(
+                children: [
                   ClipRRect(
                     child: Image.network(
                       widget.news[index].urlToImage,
@@ -154,40 +157,44 @@ class _NewsGridState extends State<NewsGrid> {
                     ),
                   ),
                   Container(
-                      margin: const EdgeInsets.only(
-                        top: 14.976,
-                        bottom: 14.976,
-                      ),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Expanded(
-                            child: Text(
-                              widget.news[index].title,
-                              style: const TextStyle(
-                                fontWeight: FontWeight.w700,
-                                fontSize: 16,
-                              ),
+                    margin: const EdgeInsets.only(
+                      top: 14.976,
+                      bottom: 14.976,
+                    ),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            widget.news[index].title,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 16,
                             ),
                           ),
-                        ],
-                      )),
+                        ),
+                      ],
+                    ),
+                  ),
                   Container(
-                      margin: const EdgeInsets.only(bottom: 12.8),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Expanded(
-                            child: Text(
-                              widget.news[index].description,
-                              style: const TextStyle(fontSize: 16),
-                            ),
+                    margin: const EdgeInsets.only(bottom: 12.8),
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Expanded(
+                          child: Text(
+                            widget.news[index].description,
+                            style: const TextStyle(fontSize: 16),
                           ),
-                        ],
-                      ))
-                ])),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
           );
         },
       );
